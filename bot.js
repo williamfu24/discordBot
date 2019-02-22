8ee09137a54782cca1b7c998c5ee0ea325843420
williamfu24 <william-fu@live.com>
2019-02-22 13:17:02 -0600

    Initial commit

---
+++ bot.js
@@ -0,0 +1,75 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./DiscordBot/auth.json'); // ./ or ../ for relative path| / for absolute path
+//Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console),{
+    colorized:true
+});
+logger.level = 'debug';
+//Init Discord Bot
+var bot = new Discord.Client({ //creates new discord client aka bot
+    token: auth.token, //use auth.json's token
+    autorun: true
+});
+bot.on('ready', function(evt){ //evt = event. When bot is ready triggers event to log information
+    logger.info("Connected");
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' -(' + bot.id ')');
+});
+bot.on('message', function(user, userID, channelID, message, evt){ //while bot is on. message function (takes user, user idm channel id, message, and is evt)
+    //Bot needs to know if it will execute a command
+    //Listen for messages starting with !
+    if (message.substring(0,1)=='!'){ //if message substring first char (0,1) ==! then
+        var args = message.substring(1).split(' '); //split message into args split on spaces after !
+        var cmd = args[0]; //cmd is first part of args so ! hi dad. cmd = hi
+
+        args = args.splice(1); //splice(int index, int howmany, itemA, itemB). index = position to add/remove (-num for from the end)
+                                //howmany = items to remove. 0 = none
+                                //item1-->itemX new items to add
+                                //args = args.splice(1) keeps only first item/gets rid of rest starting at postion 1
+        switch(cmd){ //based on cmd determines which case
+            //!ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            //More case Commands here
+        }
+    }
+});
+bot.on('Hi Dad', function(user, userID, channelID, message, evt){
+    var args = message.split(' ');
+    var cmd = args[0];
+    args = args.splice(1);
+
+    switch(cmd){
+        //'im'
+        case 'im':
+            bot.sendMessage({
+                to: channelID,
+                message: ("Hi" + args + ", I'm BEEPBOOP");
+            });
+        break;
+        case 'Im':
+            bot.sendMessage({
+                to: channelID,
+                message: ("Hi" + args + ", I'm BEEPBOOP");
+            });
+        break;
+        case "I'm":
+            bot.sendMessage({
+                to: channelID,
+                message: ("Hi" + args + ", I'm BEEPBOOP");
+            });
+        break;
+
+    }
+});
+//Install dependencies
+//npm install discord.io winston --save
+
+//Run script by executing
+//node bot.js
